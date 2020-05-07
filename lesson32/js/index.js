195a367419306904cab365e478ace23023d809ba
ivanyusyp <ivanusup@gmail.com>
2020-05-07 14:37:04 +0300

    finished task in lesson32 bu without customizing arrows

---
+++ lesson32/js/index.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper('.swiper-container', {
+	slidesPerView: 4,
+	spaceBetween: 10,
+	slidesPerGroup: 4,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});

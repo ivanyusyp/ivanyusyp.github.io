ffa252e1b65ff9bab462e377dcc0d7d7340bd669
ivanyusyp <ivanusup@gmail.com>
2020-05-13 18:10:49 +0300

    do action of button 2,3,last and before-the-last, needed to fix btn .next

--- lesson33/js/index.js
+++ lesson33/js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 	let inputData,
 		selectData,
+		firstBntValue = 1,
+		secondBtnValue = firstBntValue + 1,
+		thirdBntValue = secondBtnValue + 1,
 		pageValue = 1;
 	$(".btn").click(function nextPage(e) {
 		e.preventDefault();
@@ -19,7 +22,7 @@ $(document).ready(function () {
 				if (searchResult.Response === "True") {
 					searchArray.map(item => {
 						$(".results").append(
-							`<div class='result-div${item.imdbID} result-div'><img class="result-img" src="${item.Poster}" alt="">
+							`<div class='result-div${item.imdbID} result-div'><p class="film-title">${item.Title}</p><img class="result-img" src="${item.Poster}" alt="">
 							<button id="${item.imdbID}" class="details-btn">Details</button>
 							</div>`
 						);
@@ -30,31 +33,66 @@ $(document).ready(function () {
 								success: function (results) {
 									console.log(results);
 									$(`.result-div${item.imdbID}`).append(
-										`<p>${results.Plot}</p>`
+										`<p class="plot${item.imdbID}">${results.Plot}</p>`
 									)
-
 								}
 							});
 						});
 					});
-					const totalResult = searchResult.totalResults;
+					const totalResult = searchResult.totalResults,
+						lastPage = Math.ceil(totalResult / 10);
 					if (+(totalResult) > 10) {
+						console.log(lastPage);
 						$('.pagination').append(
-							`<button class="previous-btn"><<</button>
-							<button class="pagination-btn" value="1">1</button>
+							`
+							<button class="previous-btn"><<</button>
+							<button class="first-btn" value="1">1</button>
+							<button class="second-btn" value="2">2 </button>
+							<button class="third-btn" value="3">3</button>
+							<button class="next" >...</button>
+							<button class=" before-the-last-btn" value="L-1">${lastPage - 1}</button>
+							<button class=" last-btn" value="L">${lastPage}</button>
 							<button class="next-btn">>></button>
-							<button class="last-btn">last</button>`
+							`
 						);
 					}
 					console.log(`Current value: ${pageValue}`);
-					$(`.pagination-btn`).click(function (e) {
+					$(`.first-btn`).click(function (e) {
 						e.preventDefault();
-						let bntValue = $(".pagination-btn").val();
-						console.log(`btnvalue : ${bntValue}`);
-						pageValue = bntValue;
+						pageValue = firstBntValue;
 						$(".results").empty();
 						$(".pagination").empty();
 						nextPage(e);
+					});
+					$(`.second-btn`).click(function (e) {
+						e.preventDefault();
+						pageValue = secondBtnValue;
+						$(".results").empty();
+						$(".pagination").empty();
+						nextPage(e);
+					});
+					$(`.third-btn`).click(function (e) {
+						e.preventDefault();
+						pageValue = thirdBntValue;
+						$(".results").empty();
+						$(".pagination").empty();
+						nextPage(e);
+					});
+					$(`.next`).click(function (e) {
+						e.preventDefault();
+						$(".second-btn").empty();
+						$(".third-btn").empty();
+						console.log(secondBtnValue);
+						console.log(thirdBntValue);
+						let secondBtnCurrentValue = +(thirdBntValue + 1);
+						let secondBtnCurrentText = +(thirdBntValue + 1);
+						let thirdBtnCurrentValue = +(thirdBntValue + 2);
+						let thirdBtnCurrentText = +(thirdBntValue + 2);
+						$(`.second-btn`).val(`${secondBtnCurrentValue}`);
+						$(`.second-btn`).text(`${secondBtnCurrentText}`);
+						$(`.third-btn`).val(`${thirdBtnCurrentValue}`);
+						$(`.third-btn`).text(`${thirdBtnCurrentText}`);
+						console.log(`2v = ${secondBtnCurrentValue} \n2t = ${secondBtnCurrentText}`)
 					});
 					console.log(`New value: ${pageValue}`);
 					$(`.next-btn`).click(function (e) {
@@ -78,6 +116,22 @@ $(document).ready(function () {
 							$(".pagination").empty();
 							nextPage(e);
 						}
+					});
+					$(`.last-btn`).click(function (e) {
+						e.preventDefault();
+						let endPage = lastPage;
+						pageValue = endPage;
+						$(".results").empty();
+						$(".pagination").empty();
+						nextPage(e);
+					});
+					$(`.before-the-last-btn`).click(function (e) {
+						e.preventDefault();
+						let endPage = lastPage - 1;
+						pageValue = endPage;
+						$(".results").empty();
+						$(".pagination").empty();
+						nextPage(e);
 					});
 				} else {
 					$(".results").append(

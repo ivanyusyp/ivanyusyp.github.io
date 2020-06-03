f481ba9e0b786930ad0c86084f312138d280a4ca
ivanyusyp <ivanusup@gmail.com>
2020-06-03 15:36:36 +0300

    changed config for webpack

---
+++ react-app/src/components/app/App.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.component.scss';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello</h1>d
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ react-app/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
+};

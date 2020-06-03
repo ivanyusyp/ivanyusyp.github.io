b8bffacb18868fb1f700c0199730f99a4b023c73
ivanyusyp <ivanusup@gmail.com>
2020-06-03 18:46:47 +0300

    create structure of header and added icon-moon font

---
+++ car-store/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: './index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		})
+	],
+	devServer: {
+		port: 4200
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.(png|jpeg|gif|svg)$/,
+				loader: 'file-loader',
+				options: {
+					outputPath: 'img',
+				},
+			},
+		]
+	}
+};

a858e5a43b35345b14d272a0985cc4d02f47d8fa
ivanyusyp <ivanusup@gmail.com>
2020-06-11 22:16:50 +0300

    added secon filter for bodystyles

--- car-store/search/js/index.js
+++ car-store/search/js/index.js
@@ -1,10 +1,14 @@
 const search = document.getElementById("searchBtn");
 const output = document.getElementById("searchOutput");
+const categoriesSelect = document.getElementById('categories');
+const bodyStyles = document.querySelector('#bodystyles');
 const API_KEY = 'wF83X8QPO0vZqpQ50l8OfCcdjGSNU74TbPOTADl3';
 const BASE_URL = 'https://developers.ria.com/auto/';
+const preloader = document.querySelector('#loader');
+const currentUrl = window.location.href;
 let carBlockElements;
 const addImgToCarBlocks = ({ photoData: { seoLinkB } }) => {
-	console.log('photoData, ', seoLinkB);
+	// console.log('photoData, ', seoLinkB);
 	const imgSrc = seoLinkB;
 	const img = document.createElement('img');
 	img.setAttribute('class', 'cars__img');
@@ -15,7 +19,7 @@ const addImgToCarBlocks = ({ photoData: { seoLinkB } }) => {
 
 }
 const addTitleAndPrice = ({ title, USD }) => {
-	console.log('title', title, 'USD', USD);
+	// console.log('title', title, 'USD', USD);
 	const carName = document.createElement('p');
 	const carCost = document.createElement('p');
 	carCost.innerText = 'Вартість: ' + '$' + USD;
@@ -26,7 +30,7 @@ const addTitleAndPrice = ({ title, USD }) => {
 	carBlockElements.appendChild(carCost);
 }
 const addRaceAndYear = ({ autoData: { raceInt, year } }) => {
-	console.log('race', raceInt, "year", year);
+	// console.log('race', raceInt, "year", year);
 	const raceValue = document.createElement('p');
 	raceValue.setAttribute('class', 'cars__race');
 	raceValue.innerText = 'Пробіг: ' + raceInt + "тис. км.";
@@ -37,7 +41,7 @@ const addRaceAndYear = ({ autoData: { raceInt, year } }) => {
 	carBlockElements.appendChild(yearOfManufacture);
 }
 const createContainerBlocks = ({ secureKey, ...data }) => {
-	console.log('userId', secureKey);
+	// console.log('userId', secureKey);
 	const carBlock = document.createElement('div');
 	carBlock.setAttribute('class', 'cars__blocks');
 	carBlock.setAttribute('id', `${secureKey}`);
@@ -52,7 +56,7 @@ const fetchCarDataById = (item) => {
 		.then(function (response) { return response.json(); })
 		.then((data) => {
 			createContainerBlocks(data);
-			console.log(data, "DATA_FOR_EACH");
+			// console.log(data, "DATA_FOR_EACH");
 		})
 }
 const fetchForCarsCategories = () => {
@@ -63,25 +67,57 @@ const fetchForCarsCategories = () => {
 		})
 }
 const setOptionForCategories = ({ name, value }) => {
-	const categoriesSelect = document.getElementById('categories');
+
 	const categoriesOption = document.createElement('option');
 	categoriesOption.setAttribute('name', `${name}`);
 	categoriesOption.setAttribute('value', `${value}`);
 	categoriesOption.innerText = name;
 	categoriesSelect.appendChild(categoriesOption);
 }
+const fetchOnChangeCategories = (e) => {
+	fetch(`${BASE_URL}search?api_key=${API_KEY}&category_id=${e}`)
+		.then(function (response) { return response.json(); })
+		.then(function (data) {
+			const dataResult = data.result;
+			const resultSearchResult = dataResult.search_result;
+			const autoIdArray = resultSearchResult.ids;
+			autoIdArray.forEach(item => {
+				fetchCarDataById(item);
+			})
+		})
+		.finally(() => {
+			preloader.style.display = 'none';
+		})
+}
+const fetchOnChangeBodyStyles = (e) => {
+	fetch(`${BASE_URL}search?api_key=${API_KEY}&category_id=${e}&bodystyle__id=${e}`)
+}
+const fetchForBodyStyles = (e) => {
+	fetch(`${BASE_URL}categories/${e}/bodystyles?api_key=${API_KEY}`)
+		.then(function (response) { return response.json(); })
+		.then((data) => {
+			data.forEach(item => {
+				setOptionForBodyStyles(item);
+			})
+			console.log(data, "DATA_FOR_BODYSTYLES");
+		})
+}
+
+const setOptionForBodyStyles = ({ name, value }) => {
+	const bodyStylesOption = document.createElement('option');
+	bodyStylesOption.setAttribute('name', `${name}`);
+	bodyStylesOption.setAttribute('value', `${value}`);
+	bodyStylesOption.innerText = name;
+	bodyStyles.appendChild(bodyStylesOption);
+}
 
 window.onload = (e) => {
 	fetch(`${BASE_URL}search?api_key=${API_KEY}&category_id=1`)
 		.then(function (response) { return response.json(); })
 		.then(function (data) {
-			console.log(data, "DATA");
 			const dataResult = data.result;
-			console.log(dataResult, 'DATARESULT');
 			const resultSearchResult = dataResult.search_result;
-			console.log(resultSearchResult, 'ResSearchRes');
 			const autoIdArray = resultSearchResult.ids;
-			console.log(autoIdArray, "IDs");
 			autoIdArray.forEach(item => {
 				fetchCarDataById(item);
 			})
@@ -89,9 +125,27 @@ window.onload = (e) => {
 	fetch(`${BASE_URL}categories/?api_key=${API_KEY}`)
 		.then(function (response) { return response.json(); })
 		.then((data) => {
-			console.log(data, "DATA_FOR_CATEGORIES");
 			data.forEach(item => {
 				setOptionForCategories(item);
 			})
 		})
 }
+if (categoriesSelect) categoriesSelect.addEventListener('change', (e) => {
+	preloader.style.display = "block";
+	output.innerHTML = '';
+	bodyStyles.innerHTML = "";
+	fetchOnChangeCategories(e.target.value);
+	console.log(e.target.value);
+	fetchForBodyStyles(e.target.value);
+	console.log(currentUrl, '===================CURRENT URL========================');
+})
+if (bodyStyles) bodyStyles.addEventListener('change', (e) => {
+	preloader.style.display = "block";
+	output.innerHTML = '';
+	bodyStyles.innerHTML = "";
+	// fetch(`${BASE_URL}search?api_key=${API_KEY}&category_id=1&bodystyle__id=3`)
+	// 	.then(function (response) { return response.json(); })
+	// 	.then((data) => {
+	// 		console.log(data, "DATA_FOR_BODYSTYLE3");
+	// 	})
+})

2ac60937bdb4add060f897278bc0c7a8f9720c57
ivanyusyp <ivanusup@gmail.com>
2020-06-19 17:10:00 +0300

    added transition for header, footer and main page elements

--- car-store/webpack.config.js
+++ car-store/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	entry: {
@@ -13,6 +13,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	plugins: [
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: './index.html',
 		}),

de11be5296184a6c8c0b5e525a22cd8494dcf705
ivanyusyp <ivanusup@gmail.com>
2020-06-19 17:14:52 +0300

    fixed dist output files

--- car-store/webpack.config.js
+++ car-store/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	entry: {
@@ -13,7 +12,6 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	plugins: [
-		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: './index.html',
 		}),

f84852a59ba210cadd1c244e3dfd8c2fc649b7e9
ivanyusyp <ivanusup@gmail.com>
2020-06-28 22:43:41 +0300

    added fitlers for category and price but they worked only one by one

--- TEST_TASK/TEST_TASK/js/index.js
+++ TEST_TASK/TEST_TASK/js/index.js
@@ -4,38 +4,65 @@ const inputElementsArray = Array.from(inputElements);
 inputElementsArray.forEach(item => {
 	item.min = 0;
 });
-const breakfastItems = document.getElementsByClassName('breakfast-item');
-const breakfastItemsArray = Array.from(breakfastItems);
-const garnishItems = document.getElementsByClassName('garnish-item');
-const garnishItemsArray = Array.from(garnishItems);
-const soupItems = document.getElementsByClassName('soup-item');
-const soupItemsArray = Array.from(soupItems);
-const allItems = document.getElementsByClassName('product-box__item');
-const allItemsArray = Array.from(allItems);
+let priceTargetValue,
+	categoryTargetValue;
 
-const categorySelect = document.getElementById('category-select');
+const categorySelect = document.querySelector('#category-select');
 const priceSelect = document.querySelector('#price-select');
 console.log(priceSelect);
+const filteredByPrice = (priceTargetValue) => {
+	const priceItems = document.querySelectorAll("[data-dish-price]");
+	const priceItemsArray = Array.from(priceItems);
+	if (priceTargetValue != 0) {
+		priceItemsArray.forEach(item =>
+			item.style.display = 'none'
+		)
+		const filtered = priceItemsArray.filter(element => +(element.dataset.dishPrice) <= priceTargetValue);
+		console.log(filtered);
+		filtered.forEach(item =>
+			item.style.display = 'flex'
+		)
+	}
+	else {
+		priceItemsArray.forEach(item =>
+			item.style.display = 'flex'
+		)
+		console.log(priceItemsArray);
+	}
+};
+const filteredByType = (categoryTargetValue) => {
+	const typeItems = document.querySelectorAll("[data-dish-type]");
+	const typeItemsArray = Array.from(typeItems);
+	if (categoryTargetValue != 0) {
+		typeItemsArray.forEach(item =>
+			item.style.display = 'none'
+		)
+		const filtered = typeItemsArray.filter(element => +(element.dataset.dishType) == categoryTargetValue);
+		console.log(filtered);
+		filtered.forEach(item =>
+			item.style.display = 'flex'
+		)
+	}
+	else {
+		typeItemsArray.forEach(item =>
+			item.style.display = 'flex'
+		)
+		console.log(typeItemsArray);
+	}
+};
 if (priceSelect) priceSelect.addEventListener('change', (e) => {
 	e.preventDefault();
 	console.log(e.target.value);
-	if (e.target.value == 0) {
-		console.log(allItemsArray);
-		allItemsArray.forEach(item => {
-			item.setAttribute('class', 'display-flex');
-		})
-	}
-	if (e.target.value == 30) {
-		allItemsArray.forEach(item => {
-			console.log(item.getElementsByClassName('product-box__item'));
-			// if (item.value() >= 30) {
-			// 	item.setAttribute('class', 'display-flex');
-			// } else {
-			// 	item.setAttribute('class', 'display-none');
-			// }
-		})
-	}
+	priceTargetValue = e.target.value;
+	filteredByPrice(priceTargetValue);
+	return priceTargetValue;
 })
-
+if (categorySelect) categorySelect.addEventListener('change', (e) => {
+	e.preventDefault();
+	console.log(e.target.value);
+	categoryTargetValue = e.target.value;
+	filteredByType(categoryTargetValue);
+	return categoryTargetValue;
+})
 console.log(inputElements);
 console.log(inputElementsArray);

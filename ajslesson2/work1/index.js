bf4e1ca566241ae6c798d0d6692db863d42bdd97
unknown <ivanusup@gmail.com>
2020-07-16 12:01:15 +0300

    finished workw in hw, created function sum(a)(b)

--- ajslesson2/work1/index.js
+++ ajslesson2/work1/index.js
@@ -8,14 +8,24 @@
 li получали его номер (let НЕ ИСПОЛЬЗОВАТЬ)
 3. Выполнить задачу 2 но при этом используя объявление
 итерационной переменной через let 
- 
 */
 
+// const list = document.getElementById("list"),
+// 	els = list.getElementsByTagName("li");
+
+// for (var i = 0, len = els.length; i < len; i++) {
+// 	els[i].onclick = function () {
+// 		alert(i);
+// 	};
+// }
 const list = document.getElementById("list"),
 	els = list.getElementsByTagName("li");
 
-for (var i = 0, len = els.length; i < len; i++) {
-	els[i].onclick = function () {
-		alert(i);
-	};
+for (var i = 0; i < els.length; i++) {
+	function getNumber() {
+		alert(i)
+	}
+	els[i].onclick = getnumber();
+
 }
+

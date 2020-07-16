7ea3f12f74e5dca1fe64f8810815f84dfe500238
unknown <ivanusup@gmail.com>
2020-07-16 14:37:38 +0300

    finished lab2

--- ajslesson2/lab/lab_2/index.js
+++ ajslesson2/lab/lab_2/index.js
@@ -1,23 +1,12 @@
-  /* Todo:
-            Есть 3 логгера, которые выводят разные заголовки и текст сообщений
-            Задача - реализовать такую же функциональность выполняемую одной функцией
-        */
-			 function log() {
-				// todo
-			}
-
-			function log1(message) {
-				let out = `LOG_1: ${message}`;
-				console.log(out);
-			}
-			function log2(message) {
-				let out = `LOG_2: ${message}`;
-				console.log(out);
-			}
-			function log3(message) {
-				let out = `LOG_3: ${message}`;
-				console.log(out);
-			}
-			log1("Message 1");
-			log2("Message 2");
-			log3("Message 3");
+/* Todo:
+					Есть 3 логгера, которые выводят разные заголовки и текст сообщений
+					Задача - реализовать такую же функциональность выполняемую одной функцией
+			*/
+function log(message) {
+	log1();
+	log2();
+	log3();
+}
+const log1 = console.log("Message 1")
+const log2 = console.log("Message 2");
+const log3 = console.log("Message 3");

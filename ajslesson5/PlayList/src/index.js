4e1432a0e2f0ebba4959251b0e2266844bb97abf
unknown <ivanusup@gmail.com>
2020-07-25 10:53:57 +0300

    finished work1 from lesson5 rewriting by class with methods

--- ajslesson5/PlayList/src/index.js
+++ ajslesson5/PlayList/src/index.js
@@ -3,12 +3,9 @@ import PlayList from "./components/playlist";
 import Movie from "./components/movie";
 
 const playList = new PlayList();
-const s1 = new Song();
-s1.toHtml('TEST', 'Tom', '03:12');
-const s2 = new Song();
-s2.toHtml('TEST 1', 'Bill', '05:22');
-const m1 = new Movie();
-m1.toHtml('Man of steel', 2012, '02:33:15');
+const s1 = new Song('TEST', 'Tom', '03:12');
+const s2 = new Song('TEST 1', 'Bill', '05:22');
+const m1 = new Movie('Man of steel', 2012, '02:33:15');
 
 
 playList.add(s1)

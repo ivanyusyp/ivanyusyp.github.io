6885653921a194ce240785cf969616120b5baccc
unknown <ivanusup@gmail.com>
2020-08-04 08:45:37 +0300

    fixed prototype.constructor and object.create in BookProduct from CDProduct to shopProduct

--- ajslesson5/ShopProduct/src/components/BookProduct.js
+++ ajslesson5/ShopProduct/src/components/BookProduct.js
@@ -1,11 +1,10 @@
 import shopProduct from './ShopProduct.js';
-import cdProduct from './CDProduct.js';
 function bookProduct(title, numPages, price) {
 	this.numPages = numPages;
 	shopProduct.call(this, title, price);
 }
-bookProduct.prototype = Object.create(cdProduct.prototype);
-bookProduct.prototype.constructor = cdProduct.prototype;
+bookProduct.prototype = Object.create(shopProduct.prototype);
+bookProduct.prototype.constructor = shopProduct.prototype;
 bookProduct.prototype.getNumPages = function () {
 	console.log('Number of pages: ' + this.numPages);
 }

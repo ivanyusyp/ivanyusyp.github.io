fcb7bd6b155fecc278f16cee2f68ff89b578b232
unknown <ivanusup@gmail.com>
2020-08-14 00:11:17 +0300

    added toggleDescription and findFilmById methods in App, create Description component where displayed films description

---
+++ project_films/part_1/client/src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react"
+import { films } from "../data"
+import FilmsList from "./films"
+import { orderBy } from 'lodash';
+
+const AppContext = React.createContext()
+export { AppContext }
+
+class App extends Component {
+	state = {
+		films: [
+		],
+	}
+	addDescField = (films) =>
+		films.map(item =>
+			item.isDescriptionVisible = false
+		)
+	componentDidMount() {
+		this.setState({
+			films: this.sortFilms(films),
+		})
+		this.addDescField(films)
+		console.log(films)
+	}
+
+	toggleFeatured = id =>
+		this.setState(({ films }) => ({
+			films: this.sortFilms(
+				films.map(item =>
+					item._id === id ? { ...item, featured: !item.featured } : item,
+				),
+			),
+		}))
+
+	toggleDescription = id => {
+		this.setState(({ films }) => ({
+			films: films.map(item =>
+				item._id === id ? { ...item, isDescriptionVisible: !item.isDescriptionVisible } : item,
+			)
+		}))
+
+
+	}
+	findFilmById = id => {
+		const currentFilm = this.state.films.find(item => item._id === id)
+		return currentFilm
+	}
+
+
+	sortFilms = films => orderBy(films, ["featured", "title"], ["desc", "asc"])
+
+	render() {
+		const { films } = this.state;
+
+		return (
+			<AppContext.Provider
+				value={{
+					toggleFeatured: this.toggleFeatured,
+					toggleDescription: this.toggleDescription,
+					findFilmById: this.findFilmById
+				}}
+			>
+				<div className="ui container mt-3">
+					<FilmsList films={films} />
+				</div>
+			</AppContext.Provider>
+		)
+	}
+}
+
+export default App

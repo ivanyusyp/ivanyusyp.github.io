bef990af65ecb97e283224e972bb4f05893c2dbe
unknown <ivanusup@gmail.com>
2020-08-14 15:03:30 +0300

    added examples for lesson12_ajs

---
+++ AdvancedJS/ReactIntro/lesson12/Example_1(start)/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Message1 from "./components/Message1";
+import Message2 from "./components/Message2";
+
+const App = () => (
+    <div className="container">
+        <h1>Hello React</h1>
+        <Message1 />
+        <Message2 />
+    </div>
+);
+
+export default App;
---
+++ AdvancedJS/ReactIntro/lesson12/Example_1(start)/src/components/Message2.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Message2 = () => <p>Hello User</p>;
+
+export default Message2;

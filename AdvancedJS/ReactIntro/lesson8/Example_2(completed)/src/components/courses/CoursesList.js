86a6b229d8b0138eb7e8f2c4885eb105fc70452d
unknown <ivanusup@gmail.com>
2020-08-14 15:00:06 +0300

    lesson8ajs

--- AdvancedJS/ReactIntro/lesson8/Example_2(completed)/src/components/courses/CoursesList.js
+++ AdvancedJS/ReactIntro/lesson8/Example_2(completed)/src/components/courses/CoursesList.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { compose } from 'recompose';
 import Course from "./Course";
 import withToggleId from "../../hoc/withToggleId";
+import withTeachers from "../../hoc/withTeachers";
 
 const CoursesList = ({ courses, ...rest }) => {
-    return (
-        <div>
-            {courses.map(course => (
-                <Course key={course._id} course={course} {...rest} />
-            ))}
-        </div>
-    );
+	return (
+		<div>
+			{courses.map(course => (
+				<Course key={course._id} course={course} {...rest} />
+			))}
+		</div>
+	);
 };
 
-export default withToggleId(CoursesList);
+const enhance = compose(
+	withToggleId,
+	withTeachers
+)
+export default enhance(CoursesList);
